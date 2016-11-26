ab80d462f287f3a6ee589105744f70194702926c
tomeroth <tomeroth@op.pl>
2016-11-26 16:14:51 +0100

    Rebuild JsonStruct

--- app/src/main/java/com/example/tomek/schedule/model/JsonStruct.java
+++ app/src/main/java/com/example/tomek/schedule/model/JsonStruct.java
@@ -1,6 +1,7 @@
 package com.example.tomek.schedule.model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Tomek on 2016-11-24.
@@ -8,6 +9,9 @@ import java.util.ArrayList;
 
 public class JsonStruct {
     private String title;
+    private String type;
+    private String classroom;
+    private String teacher;
     private String start;
     private String end;
     private String eid;
@@ -16,7 +20,11 @@ public class JsonStruct {
 //        private String backgroundColor;
 
     public JsonStruct(String _title, String _start, String _end, String _eid, String _group) {
-        this.title = _title;
+        String[] title_data = setTitles(_title);
+        title = title_data[0];
+        type = title_data[1];
+        classroom = title_data[2];
+        teacher = title_data[3];
         this.start = _start;
         this.end = _end;
         this.eid = _eid;
@@ -35,11 +43,35 @@ public class JsonStruct {
         return result;
     }
 
+    public String[] getTitleData(){
+        String[] result ={title, type, classroom, teacher};
+        return result;
+    }
+
     public String getTitle(){
         return title;
     }
 
     public String getGroup(){
         return group;
+    }
+
+    private String[] setTitles(String _from_title){
+        /**
+         * titles[0] = nazwa zajęć
+         * titles[1] = rodzaj zajęć, Cwwicz. lab
+         * titles[2] = sala, C3 210
+         * titles[3] = prowadzący, mgr inż. Franek Kimono
+         */
+        String[] titles = _from_title.split(", ");
+        System.out.println(titles.length);
+        if (titles.length == 5) {
+            // Lab i inne
+            titles[2] = titles[2].replaceAll(".*: ", ""); // grupa 1b<br/>Sala: C3 123 -> C3 123
+            titles[3] = titles[4] + " " + titles[3].replace("prowadzący: ", "");
+        }else{
+            // Wykład
+        }
+        return titles;
     }
 }
