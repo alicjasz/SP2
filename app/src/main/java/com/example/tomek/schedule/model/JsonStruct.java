ca39104681d75f7f75ef31bc4f6748c5accf1c16
tomeroth <tomeroth@op.pl>
2016-11-26 16:38:57 +0100

    Added Calendar format for start end attributes in JsonStruct

--- app/src/main/java/com/example/tomek/schedule/model/JsonStruct.java
+++ app/src/main/java/com/example/tomek/schedule/model/JsonStruct.java
@@ -1,7 +1,10 @@
 package com.example.tomek.schedule.model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by Tomek on 2016-11-24.
@@ -12,8 +15,8 @@ public class JsonStruct {
     private String type;
     private String classroom;
     private String teacher;
-    private String start;
-    private String end;
+    private Calendar start;
+    private Calendar end;
     private String eid;
     //        private String atid;
     private String group;
@@ -25,8 +28,8 @@ public class JsonStruct {
         type = title_data[1];
         classroom = title_data[2];
         teacher = title_data[3];
-        this.start = _start;
-        this.end = _end;
+        this.start = convertToDate(_start);
+        this.end = convertToDate(_end);
         this.eid = _eid;
 //            this.atid = _atid;
         this.group = _group;
@@ -36,8 +39,8 @@ public class JsonStruct {
     public ArrayList<String> getAll() {
         ArrayList<String> result = new ArrayList<String>();
         result.add(title);
-        result.add(start);
-        result.add(end);
+        result.add(start.toString());
+        result.add(end.toString());
         result.add(eid);
         result.add(group);
         return result;
@@ -56,6 +59,27 @@ public class JsonStruct {
         return group;
     }
 
+
+    private Calendar convertToDate(String _date){
+        // 2016-11-28T16:15:00+0100
+        /**
+         * Żeby coś z tego wyciągnąć
+         * start.get(Calendar.DAY_OF_WEEK)
+         * end.get(Calendar.MINUTE)
+         * Date deprecated
+         */
+        Calendar cal = Calendar.getInstance();
+        String date = _date.substring(0, _date.length()-5);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        try {
+            cal.setTime(format.parse(_date));
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return cal;
+    }
+
     private String[] setTitles(String _from_title){
         /**
          * titles[0] = nazwa zajęć
@@ -64,13 +88,12 @@ public class JsonStruct {
          * titles[3] = prowadzący, mgr inż. Franek Kimono
          */
         String[] titles = _from_title.split(", ");
-        System.out.println(titles.length);
         if (titles.length == 5) {
             // Lab i inne
             titles[2] = titles[2].replaceAll(".*: ", ""); // grupa 1b<br/>Sala: C3 123 -> C3 123
             titles[3] = titles[4] + " " + titles[3].replace("prowadzący: ", "");
         }else{
-            // Wykład
+            // Wykład jakiś jeden spiepszony, na razie zostawiam
         }
         return titles;
     }
